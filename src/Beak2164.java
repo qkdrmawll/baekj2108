dd5a1fdf11775e6cdfe19070f853923423d47807
Eunji Bang <eunji8106@naver.com>
2022-11-01 17:58:21 +0900

    Initial commit

---
+++ src/Beak2164.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Beak2164 {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        int temp;
+        int N = scanner.nextInt();
+        for(int i = 0;i<N;i++){
+            queue.offer(i+1);
+        }
+
+        while(queue.size()>1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+        System.out.println(queue.poll());
+
+    }
+}
