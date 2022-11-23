4d1eac2e5a9f77c17d87a64ab6ef2bc24ffe50f5
Eunji Bang <eunji8106@naver.com>
2022-11-23 21:07:16 +0900

    Initial commit

---
+++ src/Beak2839.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Beak2839 { //설탕배달
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        int result;
+        int count =0;
+
+        while (true){
+            if(N==4||N==7){
+                result = -1;
+                break;
+            }
+            if (N%5==0){
+                count += N/5;
+                result = count;
+                break;
+            }
+            else {
+                N -=3;
+                count++;
+               }
+        }
+        System.out.println(result);
+
+
+
+
+
+
+
+    }
+}
