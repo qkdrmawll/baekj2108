92607cc53433b5ced60f4eea5bd51f5f25abc163
Eunji Bang <eunji8106@naver.com>
2022-11-28 12:48:06 +0900

    Initial commit

---
+++ src/Beak4153.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Beak4153 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        long[] length = new long[3];
+        for (int i = 0;i<3;i++){
+            length[i] = Integer.parseInt(st.nextToken());
+        }
+        while(length[0]>0) {
+            Arrays.sort(length);
+            if(length[2]*length[2]==length[0]*length[0]+length[1]*length[1]){
+                sb.append("right").append("\n");
+            }
+            else sb.append("wrong").append("\n");
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0;i<3;i++){
+                length[i] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println(sb);
+    }
+}
