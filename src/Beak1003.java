3f4858f4318459322e49f02bdf43ce98bf69134d
Eunji Bang <eunji8106@naver.com>
2023-02-06 16:02:16 +0900

    update

--- src/Beak1003.java
+++ src/Beak1003.java
@@ -2,32 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Beak1003 { //피보나치 함수 미완성
-    static int count0 = 0;
-    static int count1 = 0;
+public class Beak1003 { //피보나치 함수
+
+    static Integer[][] arr = new Integer[41][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+
         for (int i=0;i<T;i++) {
-            count0 = 0;
-            count1 = 0;
-            fibonacci(Integer.parseInt(br.readLine()));
-            sb.append(count0).append(" ").append(count1).append("\n");
+
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            sb.append(arr[n][0]).append(" ").append(arr[n][1]).append("\n");
         }
         System.out.println(sb);
     }
 
-    public static void fibonacci(int n) {
-        if (n==0) {
-            count0++;
-        } else if (n==1) {
-            count1++;
-        } else {
-            fibonacci(n-1);
-            fibonacci(n-2);
+    public static Integer[] fibonacci(int n) {
+        if (n == 0) {
+            arr[n][0]=1;
+            arr[n][1]=0;
         }
+        else if (n == 1) {
+            arr[n][0]=0;
+            arr[n][1]=1;
+        }
+        if (arr[n][0]==null){
+            arr[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
+            arr[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
+        }
+        return arr[n];
     }
 
 }
