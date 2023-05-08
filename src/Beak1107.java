c9f5e1dc84a74e917d4089b45026db13ac85d924
Eunji Bang <eunji8106@naver.com>
2023-05-08 18:06:49 +0900

    Initial commit

--- src/Beak1107.java
+++ src/Beak1107.java
@@ -4,14 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Beak1107 { //리모컨
+    static int N;
+    static int M;
+    static int[] Xnums;
+    static boolean flag = false;
+    public static int check(int num) {
+        String num_str = String.valueOf(num);
+        int num_length = (int)(Math.log10(num)+1);
+
+        for (int i=0; i<num_length; i++) {
+            for (int j=0; j<M; j++) {
+                if (num_str.charAt(i)==(char)(Xnums[j])+'0') {
+                    flag = true;
+                }
+            }
+        }
+
+        if (flag == true) return num_length + Math.abs(N-num);
+        else return 0;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String N_str = String.valueOf(N);
+        N = Integer.parseInt(br.readLine());
         int N_length = (int)(Math.log10(N)+1);
-        int M = Integer.parseInt(br.readLine());
-        int[] Xnums = new int[M];
-        boolean flag = false;
+        int min = 500000;
+
+        M = Integer.parseInt(br.readLine());
+        Xnums = new int[M];
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<M;i++) {
@@ -22,20 +42,29 @@ public class Beak1107 { //리모컨
             System.out.println("0");
             return;
         }
-        for (int i=0; i<N_length; i++) {
-            for (int j=0; j<M; j++) {
-                if (N_str.charAt(i)==(char)(Xnums[j])+'0') {
-                    flag = true;
-                }
-            }
-        }
+
+        check(N);
+
         if (flag == false) { // 숫자버튼만으로 이동할 수 있는 경우
             System.out.println(N_length);
         }
         else { //숫자버튼 + '+''-'
-            ''
+            for(int i=N;i<=500000;i++){
+                flag = false;
+                int temp = check(i);
+                if (temp!=0 && temp<min) {
+                    min = temp;
+                }
+            }
+            for(int i=N;i>=0;i--){
+                flag = false;
+                int temp = check(i);
+                if (temp!=0 && temp<min) {
+                    min = temp;
+                }
+            }
+
+            System.out.println(min);
         }
-
-
     }
 }
