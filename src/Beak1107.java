13a090387bdcc46a55e979bd9351c33100533747
Eunji Bang <eunji8106@naver.com>
2023-05-12 16:37:57 +0900

    Initial commit

--- src/Beak1107.java
+++ src/Beak1107.java
@@ -1,70 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Beak1107 { //리모컨
-    static int N;
-    static int M;
-    static int[] Xnums;
-    static boolean flag = false;
-    public static int check(int num) {
-        String num_str = String.valueOf(num);
-        int num_length = (int)(Math.log10(num)+1);
+public class Beak1107 {  //리모컨
+    public static void main(String[] args)  {
+        Scanner scan = new Scanner(System.in);
 
-        for (int i=0; i<num_length; i++) {
-            for (int j=0; j<M; j++) {
-                if (num_str.charAt(i)==(char)(Xnums[j])+'0') {
-                    flag = true;
+        int target = scan.nextInt();
+        int m = scan.nextInt();
+
+        boolean[] broken = new boolean[10];
+        for(int i = 0; i < m; i++) {
+            int n = scan.nextInt();
+            broken[n] = true;
+        }
+
+        int result = Math.abs(target - 100); //초기값 설정
+        for(int i = 0; i <= 999999; i++) {
+            String str = String.valueOf(i);
+            int len = str.length();
+
+            boolean isBreak = false;
+            for(int j = 0; j < len; j++) {
+                if(broken[str.charAt(j) - '0']) { //고장난 버튼을 눌러야 하면
+                    isBreak = true;
+                    break; //더 이상 탐색하지 않고 빠져나온다.
                 }
             }
-        }
-
-        if (flag == true) return num_length + Math.abs(N-num);
-        else return 0;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int N_length = (int)(Math.log10(N)+1);
-        int min = 500000;
-
-        M = Integer.parseInt(br.readLine());
-        Xnums = new int[M];
-
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0; i<M;i++) {
-            Xnums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        if (N==100) { // 이동하려고 하는 채널 == 현재 채널인 경우
-            System.out.println("0");
-            return;
-        }
-
-        check(N);
-
-        if (flag == false) { // 숫자버튼만으로 이동할 수 있는 경우
-            System.out.println(N_length);
-        }
-        else { //숫자버튼 + '+''-'
-            for(int i=N;i<=500000;i++){
-                flag = false;
-                int temp = check(i);
-                if (temp!=0 && temp<min) {
-                    min = temp;
-                }
+            if(!isBreak) { //i를 누를때 고장난 버튼을 누르지 않는다면
+                int min = Math.abs(target - i) + len; //i를 누른 후(len) target까지 이동하는 횟수(target - i)
+                result = Math.min(min, result);
             }
-            for(int i=N;i>=0;i--){
-                flag = false;
-                int temp = check(i);
-                if (temp!=0 && temp<min) {
-                    min = temp;
-                }
-            }
-
-            System.out.println(min);
         }
+        System.out.println(result);
     }
 }
