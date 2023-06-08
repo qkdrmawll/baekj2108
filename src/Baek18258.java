4b6ed147fd98f90200ac6c32414459d2c467b1df
Eunji Bang <eunji8106@naver.com>
2023-06-08 16:58:50 +0900

    Queue -> Deque

--- src/Baek18258.java
+++ src/Baek18258.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Baek18258 { //큐 2
     public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<>();
+        Deque<Integer> que = new LinkedList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int last = 0;
         StringBuilder sb = new StringBuilder();
 
         for(int i=0;i<N;i++) {
@@ -20,28 +17,27 @@ public class Baek18258 { //큐 2
 
             switch (arr[0]) {
                 case "push":
-                    queue.offer(Integer.parseInt(arr[1]));
-                    last = Integer.parseInt(arr[1]);
+                    que.offer(Integer.parseInt(arr[1]));
                     break;
                 case "pop":
-                    if (!queue.isEmpty()) {
-                        sb.append(queue.poll()).append("\n");
+                    if (!que.isEmpty()) {
+                        sb.append(que.poll()).append("\n");
                     }else sb.append(-1).append("\n");
                     break;
                 case "size":
-                    sb.append(queue.size()).append("\n");
+                    sb.append(que.size()).append("\n");
                     break;
                 case "empty":
-                    if (queue.isEmpty()) sb.append(1).append("\n");
+                    if (que.isEmpty()) sb.append(1).append("\n");
                     else sb.append(0).append("\n");
                     break;
                 case "front":
-                    if (!queue.isEmpty()) sb.append(queue.peek()).append("\n");
+                    if (!que.isEmpty()) sb.append(que.peek()).append("\n");
                     else sb.append(-1).append("\n");
                     break;
                 case "back":
-                    if (queue.isEmpty()) sb.append(-1).append("\n");
-                    else sb.append(last).append("\n");
+                    if (que.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(que.peekLast()).append("\n");
                     break;
             }
 
