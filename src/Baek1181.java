c2408c2b062438e354bf3d6d1c146a191ffd1b0f
Eunji Bang <eunji8106@naver.com>
2023-08-09 11:49:52 +0900

    Initial commit

---
+++ src/Baek1181.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Baek1181 { /// 단어 정렬
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            String in = br.readLine();
+            if (!list.contains(in)) {
+                list.add(in);
+            }
+        }
+        Collections.sort(list);
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        for (int i=0;i<list.size();i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
