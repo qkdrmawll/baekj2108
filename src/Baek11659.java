9e1ef13708271c8f83f4e08db23da949cb98ad19
qkdrmawll <eunji8106@naver.com>
2024-01-18 11:00:03 +0900

    Initial commit

---
+++ src/Baek11659.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek11659 {//구간 합 구하기4
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] s = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i=1;i<=N;i++) {
+            s[i] = s[i-1] + Integer.parseInt(st.nextToken());
+        }
+        for (int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(s[end] - s[start-1]);
+        }
+    }
+}
