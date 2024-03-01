86a29dd0fd3cbf3a8b612524c22e7e1680f1de25
qkdrmawll <eunji8106@naver.com>
2024-03-01 11:54:16 +0900

    comment

--- src/Baek13023.java
+++ src/Baek13023.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Baek13023 { //ABCDE
-
+    //dfs
+    //인접 노드를 방문하면서 depth가 5 이상인 경우를 판별
     static ArrayList<Integer>[] A;
     static boolean[] visited;
     static boolean arrive;
@@ -26,10 +27,11 @@ public class Baek13023 { //ABCDE
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            // 양방향 연결 데이터 저장
             A[a].add(b);
             A[b].add(a);
         }
-
+        // 인접 노드를 방문하면서 depth>=5를 판별해야하므로 depth와 dfs, 1부터 시작
         for (int i=0;i<n;i++) {
             if (!visited[i]) {
                 dfs(i,1);
@@ -51,7 +53,7 @@ public class Baek13023 { //ABCDE
 
         for (int i : A[num]) {
             if (!visited[i]) {
-                dfs(i,depth+1);
+                dfs(i,depth+1); // 인접 노드를 depth를 늘리면서 방문
             }
         }
     }
--- src/Baek2023.java
+++ src/Baek2023.java
@@ -3,10 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baek2023 { //신기한 소수
+    // dfs 응용 문제
+    // depth를 자릿수의 개념으로 응용 & 인접 노드를 소수인 경우로 직접 설정(=가지치기)
     static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine()); // 자릿수
+        // 1자리수에서 소수가 아닌 4,6,8,9를 가지치기
         dfs(2,1);
         dfs(3,1);
         dfs(5,1);
@@ -14,13 +17,14 @@ public class Baek2023 { //신기한 소수
 
     }
     public static void dfs(int num, int depth) {
+        // 소수 판별은 전 depth에서 했기 때문에 자릿수만 확인하고 출력
         if (depth == n) {
             System.out.println(num);
             return;
         }
         for (int i=1;i<10;i++) {
             int newNum = num *10 + i;
-            if (isPrime(newNum)) {
+            if (isPrime(newNum)) { // 소수라면 자릿수를 늘림
                 dfs(newNum,depth+1);
             }
         }
